a9d3b77be0d47a2e76bebe7af4a591e3db5626d8
gitTARDIS <anja.bergmann@hotmail.com>
2016-01-30 01:02:00 +0100

    added poster

--- controller/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	//Redirects to home if user is not logged in
-	loggedIn: function(req, res, next){
-		if (req.session == 1){
-			return next();
-		}
-		res.redirect('/');
-	}
-}
